c31447d295c66d173b9800522d38dcdb56e746e4
Ivan Stefanischin <ivan@stefanischin.eu>
2026-07-09 17:30:23 +0200

    remove combo keys from alias name

--- keyboards/keebart/sofle_choc_pro/keymaps/liquid/inject.h
+++ keyboards/keebart/sofle_choc_pro/keymaps/liquid/inject.h
@@ -1,13 +1,13 @@
 switch (combo_index) {
   // paste from clipboard surrounded by markdown link with label "here"
-  case CMB_HJ_HERE:
+  case CMB_HERE:
     if (!pressed) {
       SEND_STRING("[here](" SS_LCTL("v") ")");
     }
     break;
 
   // cut out marked text and paste it surrounded by grv quotes
-  case CMB_J_GRVWP:
+  case CMB_GRVWP:
     if (!pressed) {
       SEND_STRING(SS_LCTL("x") "`" SS_LCTL("v") "`");
     }
